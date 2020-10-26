72bd6e106fdc5338d7a5c4617769f8baf2471af6
Chirag Arora <carora755@gmail.com>
2020-10-26 18:47:58 +0530

    displaying logged in user

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,4 +1,21 @@
 $(() => {
-    $('#navbar').load('/components/navbar.html')
+    $('#navbar').load('/components/navbar.html', loginIfNeeded)
     $('#footer').load('/components/footer.html')
 })
+
+function loginIfNeeded () {
+    let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+    if(currentUser === null){
+        $.post('/api/users', {}, (user) => {
+            if (user) {
+                window.alert(`Registered current user as : ${user.username}`)
+                console.log(`Registered current user as : ${user.username}`)
+                window.localStorage.user = JSON.stringify(user)
+                $('#nav-username').text(user.username)
+            }
+        })
+    }else{
+        console.log(`Resuming session as ${currentUser.username}`)
+        $('#nav-username').text(currentUser.username)
+    }
+}
