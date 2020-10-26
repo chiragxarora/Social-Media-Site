0aeafeb26f7b1fd8b2ff286c542cad1c58087382
Chirag Arora <carora755@gmail.com>
2020-10-26 19:25:17 +0530

    Displaying all the posts

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,5 +1,6 @@
 $(() => {
     $('#navbar').load('/components/navbar.html', loginIfNeeded)
+    $('#content').load('/components/all-posts.html', loadPosts)
     $('#footer').load('/components/footer.html')
 })
 
@@ -18,4 +19,25 @@ function loginIfNeeded () {
         console.log(`Resuming session as ${currentUser.username}`)
         $('#nav-username').text(currentUser.username)
     }
+}
+
+function loadPosts () {
+    $.get('/api/posts', (posts) => {
+        $('#postList').empty()
+        for(let p of posts){
+            $('#postList').append(`
+            <div class="col-4">
+                <div class="card mb-5" style="width: 18rem;">
+                    <div class="card-body">
+                    <h5 class="card-title">${p.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                    <p class="card-text">${p.body.substring(0,100)}....</p>
+                    <a href="#" class="card-link">Read More</a>
+                    <a href="#" class="card-link">Like</a>
+                    </div>
+                </div>
+            </div>
+        `)
+        }
+    })
 }
